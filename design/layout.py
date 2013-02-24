5080ac8d0f1af52f8467d87b7d1236af4884ff5e
Ted Lilley <tlilley@diditbetter.com>
2013-02-23 22:53:33 -0500

    Add shuffle method to layout with tests

--- design/layout.py
+++ design/layout.py
@@ -1,39 +1,45 @@
-import numpy as num
 import string as str_
+import random as rand
+import numpy as num
 import yaml
 from unipath import Path
 from bunch import bunchify
 
+rand.seed()
+cfg = bunchify(yaml.safe_load(Path('layout.yml').read_file()))
+
 class Layout(object):
     def __init__(self, value='qwertyuioasdfghjklpzxcvbnm', freqs=None, weights=None):
-        if not all([letter in value for letter in 'qwertyuioasdfghjklpzxcvbnm']):
+        if not all([letter in value for letter in 'qwertyuioasdfghjklpzxcvbnm']) or len(value) != 26:
             raise ValueError
         self._value = value
-        cfg = bunchify(yaml.safe_load(Path('layout.yml').read_file()))
         self._order = cfg.order
         self._freqs = freqs if freqs else cfg.freqs
-        self._freqs = self._freqs_array()
+        self._array_freqs = self._freqs_array()
         self._weights = weights if weights else cfg.weights
-        self._weights = self._weights_array()
+        self._array_weights = self._weights_array()
 
     def __str__(self):
         breaks = [9, 19]
         return '\n'.join([self._value[:breaks[0]], self._value[breaks[0]:breaks[1]], self._value[breaks[1]:]])
 
+    def __repr__(self):
+        return self._value
+
     @property
     def metric(self):
-        return num.dot(self._freqs, self._weights)
+        return num.dot(self._array_freqs, self._array_weights)
 
     @property
     def freqs(self):
-        return dict([(order, freq) for order, freq in zip(self._order, self._freqs) if freq])
+        return dict([(order, freq) for order, freq in zip(self._order, self._array_freqs) if freq])
 
     def _freqs_array(self):
         return [self._freqs[item] if item in self._freqs else 0 for item in self._order]
 
     @property
     def weights(self):
-        return dict([(order, weight) for order, weight in zip(self._order, self._weights) if weight])
+        return dict([(order, weight) for order, weight in zip(self._order, self._array_weights) if weight])
 
     def _weights_array(self):
         result = []
@@ -46,3 +52,9 @@ class Layout(object):
             else: 
                 result += [0]
         return result
+
+    def shuffle(self):
+        val_list = list(self._value)
+        rand.shuffle(val_list)
+        self._value = ''.join(val_list)
+        self._array_weights = self._weights_array()
--- design/test_layout.py
+++ design/test_layout.py
@@ -3,6 +3,8 @@ import yaml
 from unipath import Path
 from layout import *
 
+sample_layout = yaml.safe_load(Path('test_layout.yml').read_file())
+
 def test_layout():
     expected = '''\
 qwertyuio
@@ -14,7 +16,7 @@ zxcvbnm'''
     assert expected == actual
 
 def test_metric():
-    sample = yaml.safe_load(Path('test_layout.yml').read_file())
+    sample = sample_layout
     expected = 26*25/2
     actual = Layout(freqs=sample['freqs'], weights=sample['weights']).metric
     print 'Expected:\n', expected
@@ -26,7 +28,7 @@ def test_layout_valid_init():
     actual = Layout('a')
 
 def test_second_metric():
-    data = yaml.safe_load(Path('test_layout.yml').read_file())
+    data = sample_layout
     sample = {
         'value': 'qwdfkyuilastrghneopzxcvbjm'
         , 'freqs': data['second_freqs']
@@ -49,17 +51,17 @@ def test_freqs():
     assert expected == actual
 
 def test_freqs_array():
-    sample = yaml.safe_load(Path('test_layout.yml').read_file())
+    sample = sample_layout
     expected = [1]*(26*25/2)
-    actual = Layout(freqs=sample['freqs'])._freqs
+    actual = Layout(freqs=sample['freqs'])._array_freqs
     print 'Expected:\n', expected
     print '\nActual:\n', actual
     assert expected == actual
 
 def test_freqs_array_sparse():
-    sample = yaml.safe_load(Path('test_layout.yml').read_file())
+    sample = sample_layout
     expected = 2.5
-    actual = sum(Layout(freqs=sample['freqs_sparse'])._freqs)
+    actual = sum(Layout(freqs=sample['freqs_sparse'])._array_freqs)
     print 'Expected:\n', expected
     print '\nActual:\n', actual
     assert expected == actual
@@ -72,20 +74,29 @@ def test_weights():
     assert expected == actual
 
 def test_weights_array():
-    sample = yaml.safe_load(Path('test_layout.yml').read_file())
+    sample = sample_layout
     expected = [1]*(26*25/2)
-    actual = Layout(weights=sample['weights'])._weights
+    actual = Layout(weights=sample['weights'])._array_weights
     print 'Expected:\n', expected
     print '\nActual:\n', actual
     assert expected == actual
 
 def test_weights_array_sparse():
-    sample = yaml.safe_load(Path('test_layout.yml').read_file())
+    sample = sample_layout
     expected = 3
-    actual = sum(Layout(weights=sample['weights_sparse'])._weights)
+    actual = sum(Layout(weights=sample['weights_sparse'])._array_weights)
     print 'Expected:\n', expected
     print '\nActual:\n', actual
     assert expected == actual
+
+def test_shuffle():
+    expected = 0.67700000000000038
+    l = Layout()
+    l.shuffle()
+    actual = l.metric
+    print 'Expected:\n', expected
+    print '\nActual:\n', actual
+    assert expected != actual
 
 if __name__ == '__main__':
     test_second_metric()
